af884da938cdeb939e2d962bf9bc26024dc0f3aa
Роман Данилевич <dwordchanel@gmail.com>
2020-04-27 01:51:49 +0300

    Add project files.

---
+++ OOP_lab_3_15_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace OOP_lab_3_15_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader fromFile = new StreamReader("input.txt");
+
+            string str = fromFile.ReadToEnd();
+
+            string[] words = str.Split(new char[] { ' ', ':', ';', '.', ',', '?', '!', '(', ')', '{', '}', '[', ']', '@', '#', '№', '$', '^', '%', '&', '*' }, StringSplitOptions.RemoveEmptyEntries);
+
+            StreamWriter toFile = File.CreateText("output.txt");
+
+            toFile.WriteLine(words.Length);
+
+            fromFile.Close();
+
+            toFile.Close();
+        }
+    }
+}
